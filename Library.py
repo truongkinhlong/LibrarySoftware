f800ffb15032b9d4e6aa1ed0f03a344a12c52dd5
truongkinhlong <klong.truong3001@gmail.com>
2023-04-21 03:16:50 -0500

    personal

--- Library.py
+++ Library.py
@@ -1,35 +1,98 @@
 import os
 import mysql.connector
+
 #########################################################
 # Function
 #########################################################
 
 
-def front_window():
+def front_window():  # RETURN VOID
+    os.system("cls" if os.name == "nt" else "clear")  # CLEAR SCREEN
+
     print("Welcome to CodeX Library \n")
     print("Choose your login as: \n")
     print("1. Member\n")
     print("2. Staff\n")
     print("3. Admin\n")
+
+    # Choose Tittle
+    choice = input("Please ENTER your choice: ")
     while (True):
-        choice = input("Please enter your choice: ")
-        if ('1' <= choice <= '3'):
+        if ("1" <= choice <= "3" and len(choice) == 1):
             break
-    return int(choice)
+        else:
+            os.system("cls" if os.name == "nt" else "clear")  # CLEAR SCREEN
+
+            print("You Have Enter INVALID Value!\n")
+            print("Choose your login as: \n")
+            print("1. Member\n")
+            print("2. Staff\n")
+            print("3. Admin\n")
+            choice = input("Please RE-ENTER your choice: ")
+
+    # Assign tittle
+    global title
+    if choice == "1":
+        title = "Member"
+    if choice == "2":
+        title = "Staff"
+    if choice == "3":
+        title = "Admin"
 
 
-def login_UI(choice: str):
+def connect_to_database():  # Return VOID
+    global db
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="3020",
+        database="librarydatabase"
+    )
+
+    # Create a cursor object to execute SQL queries
+    global dbcursor
+    dbcursor = db.cursor()
+
+
+def check_login(email, password):  # RETURN True or False
+
+    # Execute the SELECT query to check if the email and password combination exists
+    sql = f"SELECT * FROM {title} WHERE email = %s AND password = %s"
+
+    #parameters = (email, password)
+    parameters = (email, password)
+    dbcursor.execute(sql, parameters)
+
+    # Fetch the result of the query
+    result = dbcursor.fetchone()
+
+    # Check if the result is not None (i.e., the email and password combination exists)
+    if result is not None:
+        return True
+    else:
+        return False
+
+
+def login_UI():  # return
     os.system("cls" if os.name == "nt" else "clear")  # CLEAR SCREEN
 
-    print("Please enter E-Mail and Password to login\n")
+    print(f"Please ENTER E-Mail and Password to login as {title}\n")
+
     email = input("E-Mail: ")
     password = input("Password: ")
+    while True:
+        if check_login(email, password):
+            os.system("cls" if os.name == "nt" else "clear")  # CLEAR SCREEN
+            print("Login Success!!!")
+            break
+        else:
+            os.system("cls" if os.name == "nt" else "clear")  # CLEAR SCREEN
+            print("Email and Password is invalid\n")
+            print("Please Re-Enter\n")
+            email = input("E-Mail: ")
+            password = input("Password: ")
 
-    # return (valid, choice)
 
-
-#########################################################
-# MAIN
-#########################################################
-temp = front_window()
-login_UI(temp)
+connect_to_database()
+front_window()
+login_UI()
--- libraryInsertDB.sql
+++ libraryInsertDB.sql
@@ -1,16 +1,21 @@
 USE libraryDatabase;
 
-INSERT INTO Admin (adminID, fName, lName, email, password)
-VALUES('AD001', 'Kinh', 'Truong', 'truongkinhlong@gmail.com', 'BLrider301!'); 
+INSERT IGNORE INTO Admin (adminID, fName, lName, email, password)
+VALUES
+	('AD000', 'Test', 'Test', 'ADTest@Test.com', 'TestAD'), 
+	('AD001', 'Kinh', 'Truong', 'truongkinhlong@gmail.com', 'BLrider301!'); 
 
-INSERT INTO Staff (staffID, fName, lName, email, password)
-VALUES('ST001', 'Adrian', 'Nguyen', 'ANguyen@gmail.com', 'AdNg001'),
+INSERT IGNORE  INTO Staff (staffID, fName, lName, email, password)
+VALUES
+	('ST000', 'Test', 'Test', 'STTest@Test.com', 'TestST'),
+	('ST001', 'Adrian', 'Nguyen', 'ANguyen@gmail.com', 'AdNg001'),
 	('ST002', 'Cerian', 'Jaime', 'CJaime@gmail.com', 'CeJa002'),
 	('ST003', 'Nathan', 'Nguyen', 'NNguyen@gmail.com', 'NaNg003'),
 	('ST004', 'Cooper', 'Tran', 'CTran@gmail.com', 'CoTr004'); 
     
-INSERT INTO Member (memberID, fName, lName, email, password)
+INSERT IGNORE  INTO Member (memberID, fName, lName, email, password)
 VALUES
+	('AD000', 'Test', 'Test', 'Test@Test.com', 'TestAD'),
     ('MB001', 'Alice', 'Jones', 'alice.jones@example.com', 'password1'),
     ('MB002', 'Bob', 'Smith', 'bob.smith@example.com', 'password2'),
     ('MB003', 'Charlie', 'Brown', 'charlie.brown@example.com', 'password3'),
@@ -21,3 +26,5 @@ VALUES
     ('MB008', 'Henry', 'Kim', 'henry.kim@example.com', 'password8'),
     ('MB009', 'Isabella', 'Chen', 'isabella.chen@example.com', 'password9'),
     ('MB010', 'James', 'Liu', 'james.liu@example.com', 'password10');
+    
+
